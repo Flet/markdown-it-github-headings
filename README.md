e8217bf08c42913027555cd33b9e0e8185bb7d1c
Dan Flettre <flettre@gmail.com>
2017-04-27 23:51:36 -0500

    init

---
+++ README.md
@@ -0,0 +1,34 @@
+# markdown-it-github-headings
+
+Add GitHub style anchor tags to headers
+
+[![npm][npm-image]][npm-url]
+[![travis][travis-image]][travis-url]
+[![standard][standard-image]][standard-url]
+
+[npm-image]: https://img.shields.io/npm/v/markdown-it-github-headings.svg?style=flat-square
+[npm-url]: https://www.npmjs.com/package/markdown-it-github-headings
+[travis-image]: https://img.shields.io/travis/Flet/markdown-it-github-headings.svg?style=flat-square
+[travis-url]: https://travis-ci.org/Flet/markdown-it-github-headings
+[standard-image]: https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat-square
+[standard-url]: http://npm.im/standard
+
+## Install
+
+```
+npm install markdown-it-github-headings
+```
+
+## Usage
+
+```js
+var markdownItGithubAnchors = require('markdown-it-github-headings')
+```
+
+## Contributing
+
+Contributions welcome! Please read the [contributing guidelines](CONTRIBUTING.md) first.
+
+## License
+
+[ISC](LICENSE.md)
---
+++ test/index.js
@@ -0,0 +1,70 @@
+var test = require('tape')
+
+const md = require('markdown-it')
+const anchor = require('..')
+const svg = anchor.svgLinkIconText
+
+test('works as expected', function (t) {
+  var result = md().use(anchor).render('# Hello')
+  var expectedResult = `<h1><a id="user-content-hello" class="anchor" href="#hello" aria-hidden="true">${svg}</a>Hello</h1>\n`
+  t.equals(result, expectedResult, 'works')
+  t.end()
+})
+
+test('multiple headers', function (t) {
+  var input = `
+## Hello
+### World
+`
+
+  var expectedResult = `<h2><a id="user-content-hello" class="anchor" href="#hello" aria-hidden="true">${svg}</a>Hello</h2>
+<h3><a id="user-content-world" class="anchor" href="#world" aria-hidden="true">${svg}</a>World</h3>
+`
+  var result = md().use(anchor).render(input)
+  t.equals(result, expectedResult, 'works')
+  t.end()
+})
+
+test('duplicate headers', function (t) {
+  var input = `
+# Hello
+## Hello
+### Hello
+`
+
+  var expectedResult = `<h1><a id="user-content-hello" class="anchor" href="#hello" aria-hidden="true">${svg}</a>Hello</h1>
+<h2><a id="user-content-hello-1" class="anchor" href="#hello-1" aria-hidden="true">${svg}</a>Hello</h2>
+<h3><a id="user-content-hello-2" class="anchor" href="#hello-2" aria-hidden="true">${svg}</a>Hello</h3>
+`
+  var result = md().use(anchor).render(input)
+  t.equals(result, expectedResult, 'works')
+  t.end()
+})
+
+test('prefix: foo-', function (t) {
+  var result = md().use(anchor, {prefix: 'foo-'}).render('# Hello')
+  var expectedResult = `<h1><a id="foo-hello" class="anchor" href="#hello" aria-hidden="true">${svg}</a>Hello</h1>\n`
+  t.equals(result, expectedResult, 'works')
+  t.end()
+})
+
+test('prefixHeadingIds: false', function (t) {
+  var result = md().use(anchor, {prefixHeadingIds: false}).render('# Hello')
+  var expectedResult = `<h1><a id="hello" class="anchor" href="#hello" aria-hidden="true">${svg}</a>Hello</h1>\n`
+  t.equals(result, expectedResult, 'works')
+  t.end()
+})
+
+test('prefixHeadingIds: false', function (t) {
+  var result = md().use(anchor, {prefixHeadingIds: false}).render('# Hello')
+  var expectedResult = `<h1><a id="hello" class="anchor" href="#hello" aria-hidden="true">${svg}</a>Hello</h1>\n`
+  t.equals(result, expectedResult, 'works')
+  t.end()
+})
+
+test('enableHeadingLinkIcons: false', function (t) {
+  var result = md().use(anchor, {enableHeadingLinkIcons: false}).render('# Hello')
+  var expectedResult = `<h1><a id="user-content-hello" class="anchor" href="#hello" aria-hidden="true"></a>Hello</h1>\n`
+  t.equals(result, expectedResult, 'works')
+  t.end()
+})
